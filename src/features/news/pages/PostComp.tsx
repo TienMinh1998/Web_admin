31277c3e3b8a9eecc2bae161106a1a760afece8d
Tran Dinh <dinh53050@gmail.com>
2023-05-25 21:43:04 +0700

    update

--- src/features/news/pages/PostComp.tsx
+++ src/features/news/pages/PostComp.tsx
@@ -1,10 +1,12 @@
-import { Button, Popconfirm } from 'antd';
+import { Button, Popconfirm, Tag, Tooltip } from 'antd';
 import React from 'react';
+import { GrCircleInformation } from 'react-icons/gr';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { PROTECTED_ROUTES_PATH } from 'routes/RoutesPath';
 import { requestDeletePost } from '../api/post.api';
 import '../style.css';
+
 type Props = { data: any; openModalUpdate?: (id: string) => void; fetchDataSource: any };
 
 export const PostComp: React.FC<Props> = ({ data, openModalUpdate, fetchDataSource }) => {
@@ -26,30 +28,32 @@ export const PostComp: React.FC<Props> = ({ data, openModalUpdate, fetchDataSour
 
   return (
     <div className="col-span-12 tablet:col-span-4 laptop:col-span-3 desktop:col-span-2 bg-white shadow-xl flex flex-col justify-between rounded-md text-base">
-      <div className="w-full aspect-square p-1">
+      <div
+        className="w-full aspect-square p-1 cursor-pointer"
+        onClick={() => {
+          goToDetail(data?.id);
+        }}>
         <img src={data?.image} className="w-full h-full aspect-square object-cover rounded-lg" />
       </div>
 
-      <div className="py-1 px-2 h-full title-item">
+      <div
+        className="py-1 px-2 h-full title-item cursor-pointer"
+        onClick={() => {
+          goToDetail(data?.id);
+        }}>
         <span className="font-bold block-ellipsis" title={data?.title}>
           {data?.title}
         </span>
       </div>
 
-      <div className="py-1 px-2 h-full sub-title">
-        <span className=" block-ellipsis" title={data?.definetion}>
-          {data?.definetion}
-        </span>
+      <div className="p-2 flex items-center justify-between mt-2">
+        {/* <Rate disabled defaultValue={2} /> */}
+        <Tag color="blue">7.5</Tag>
+        <Tooltip title={data?.definetion}>
+          <GrCircleInformation className="text-primary-color cursor-pointer" />
+        </Tooltip>
       </div>
       <div className="flex justify-center border-t p-3 w-full">
-        <Button
-          className="default_button"
-          onClick={() => {
-            goToDetail(data?.id);
-          }}>
-          Xem
-        </Button>
-
         <Button className="delete-button">
           <Popconfirm
             placement="bottom"
