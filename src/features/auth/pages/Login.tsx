f9eec5ea790fa7d60716c7f8961e2282975057d0
TienMinh <83183708+TienMinh1998@users.noreply.github.com>
2023-06-07 15:17:15 +0700

    fix login page

--- src/features/auth/pages/Login.tsx
+++ src/features/auth/pages/Login.tsx
@@ -9,7 +9,9 @@ import { Head } from 'shared/container/Head';
 import { CliCookieService, CLI_COOKIE_KEYS } from 'shared/services/cli-cookie';
 import * as Yup from 'yup';
 import { requestLogin } from '../api/auth.api';
-import './login.css'
+import '../../../Style/sb-admin-2.min.css'
+import '../../../Style/sb-admin-2.css'
+
 export const Login = () => {
   const navigate: NavigateFunction = useNavigate();
   const formik = useFormik({
@@ -32,74 +34,78 @@ export const Login = () => {
     }
   });
   return (
-
     <ContainerAuth>
-      <div className='main'>
-        <Head title="VTH-Sing in" />
-        <div className="container">
-          <div className='login-item'>
-            <img className='my-image' alt="logo" src={R.images.logo_ver2} />
-          </div>
-          <div className='login-item'>
-            <div className='login-item-main'>
-              <form onSubmit={formik.handleSubmit}>
-                <div>
-                  <div className='row-item'>
-                    <h3 className='row-item-title'>Đăng nhập</h3>
-                    <h4 className='row-item-title-detail'>Đăng nhập để sử dụng hệ thống</h4>
-                  </div>
-
-                  <div className="row-item">
-                    <span className='row-icon'>
-                      <i className="fa-solid fa-user"></i>
-                    </span>
-                    <input
-                      className='input'
-                      placeholder="Enter user name"
-                      id="userName"
-                      name="userName"
-                      onChange={formik.handleChange}
-                      value={formik.values.userName}
-                    />
-                    <div className="text-red-500 mt-1">
-                      {formik.errors.userName && formik.touched.userName && (
-                        <p>{formik.errors.userName}</p>
-                      )}
-                    </div>
-                  </div>
-                  <div className="row-item">
-                    <span className='row-icon'>
-                      <i className="fa-solid fa-lock"></i>
-                    </span>
-                    <input
-                      className='input'
-                      placeholder="Nhập mật khẩu"
-                      id="password"
-                      name="password"
-                      type="password"
-                      onChange={formik.handleChange}
-                      value={formik.values.password}
-                    />
-                    <div>
-                      {formik.errors.password && formik.touched.password && (
-                        <p>{formik.errors.password}</p>
-                      )}
+      <div className="container">
+        {/* Outer Row */}
+        <div className="row justify-content-center">
+          <div className="col-xl-10 col-lg-10 col-md-9">
+            <div className="card o-hidden border-0 shadow-lg my-5">
+              <div className="card-body p-0">
+                {/* Nested Row within Card Body */}
+                <div className="row">
+                  <div className="col-lg-6 d-none d-lg-block bg-login-image" />
+                  <div className="col-lg-6">
+                    <div className="p-5">
+                      <div className="text-center">
+                        <h1 className="h4 text-gray-900 mb-4">Welcome back to Admin</h1>
+                        <h3 className="h6 text-gray-900 mb-4">Login to use software</h3>
+                      </div>
+                      <form className="login-item" onSubmit={formik.handleSubmit}>
+                        <div className="form-group">
+                          <input className="form-control form-control-user"
+                            id="userName"
+                            name="userName"
+                            onChange={formik.handleChange}
+                            value={formik.values.userName}
+                            placeholder="Enter Email Address..." />
+                        </div>
+                        <div className="form-group">
+                          <input type="password" className="form-control form-control-user"
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            onChange={formik.handleChange}
+                            value={formik.values.password}
+                          />
+                        </div>
+                        <div className="form-group">
+                          <div className="custom-control custom-checkbox small">
+                            <input type="checkbox" className="custom-control-input" id="customCheck" />
+                            <label className="custom-control-label" htmlFor="customCheck">Remember
+                              Me</label>
+                          </div>
+                        </div>
+                        <Button
+                          className='btn btn-primary btn-user btn-block'
+                          style={{ width: '100%' }}
+                          htmlType="submit"> Login
+                        </Button>
+                        <hr />
+                        <a href="index.html" className="btn btn-google btn-user btn-block">
+                          <i className="fab fa-google fa-fw" /> Login with Google
+                        </a>
+                        <a href="index.html" className="btn btn-facebook btn-user btn-block">
+                          <i className="fab fa-facebook-f fa-fw" /> Login with Facebook
+                        </a>
+                      </form>
+                      <hr />
+                      <div className="text-center">
+                        <a className="small" href="forgot-password.html">Forgot Password?</a>
+                      </div>
+                      <div className="text-center">
+                        <a className="small" href="register.html">Create an Account!</a>
+                      </div>
                     </div>
                   </div>
                 </div>
-                <div className='button-login'>
-                  <Button
-                    style={{ width: '100%' }}
-                    htmlType="submit"> Đăng nhập
-                  </Button>
-                </div>
-              </form>
+              </div>
             </div>
-
           </div>
         </div>
       </div>
     </ContainerAuth>
+
+
 
   );
 };
